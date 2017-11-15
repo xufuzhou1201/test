ea84f0b6f1e11b120f1691c2d41b82bac4f2de3a
xufuzhou1201 <xufuzhou_1201@163.com>
2017-11-15 16:33:15 +0800

    2017-11-15

---
+++ JavaDemo/src/com/jingrui/Employ.java
@@ -0,0 +1,10 @@
+package com.jingrui;
+
+public class Employ {
+	private int mm=3;
+	
+	public void test() {
+		this.mm=5;
+	}
+
+}
---
+++ JavaDemo/src/com/jingrui/User.java
@@ -0,0 +1,14 @@
+package com.jingrui;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int d=1;
+		int f=2;
+		int g=3;
+		
+	}
+
+}
